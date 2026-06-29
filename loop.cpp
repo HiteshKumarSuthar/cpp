c1c60cc1c36057a0963ca690e6e337e2955c74d9
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-06-29 13:41:42 +0530

    Loop concept and practice question

---
+++ loop.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    /*Type of Loop in C++
+    1. For loop
+    2. While loop
+    3. do-while loop
+    */
+
+
+    /*For loop in C++*/
+    //Syntax for For loop
+    //for(initialization; condition; updation)
+    // {
+    //      loop body(C++ code);
+    // }
+
+    for(int i = 1; i <= 100; i++)
+    {
+        cout<<i<<endl;
+    }
+
+    //Example of infinite for loop
+    for(int i = 0; 34 <= 40; i++)
+    {
+        cout<<i<<endl;
+    }
+
+
+    /*Whiel loop in C++*/
+    //Syntax for While loop
+    // while(condition)
+    // {
+    //     C++ statement;
+    //     updation;
+    // }
+
+    //Printing number 1 to 50
+    int i = 1;
+    while(i <= 50){
+        cout<<i<<endl;
+        i++;
+    }
+
+    //Example of infinite while loop
+    int i = 1;
+    while(true){
+        cout<<i<<endl;
+        i++;
+    }
+
+
+    //Syntax for do-while loop
+    // do
+    // {
+    //     statement;
+    //      updation;
+    // }while(condition);
+
+    //Printing number 1 to 60 for do-while loop
+    int i = 1;
+    do
+    {
+        cout<<i<<endl;
+        i++;
+    }while(i <= 60);
+    
+
+    /*PRACTICE-QUESTION*/
+    //Q-1.Print the multiplication table of 6 using a for loop
+    for(int i = 1; i <= 10; i++)
+    {
+        cout<<i * 6<<endl;
+    }
+
+    //Q-2.Print all even numbers from 1 to 100.
+    for(int i = 1; i <= 100; i++)
+    {
+        if(i % 2 == 0){
+            cout<<"Even number"<<i<<endl;
+        }
+
+    }
+
+
+    //Q-3.Print all odd numbers from 1 to 100.
+    for(int i = 1; i <= 100; i++)
+    {
+        if(i % 2 != 0){
+            cout<<"Odd Number"<<i<<endl;
+        }
+    }
+
+
+    //Q-4.Find the sum of numbers from 1 to N.
+    int N, sum = 0;
+    cout<<"Enter the number : "<<endl;
+    cin>>N;
+    
+    for(int i = 1; i <= N; i++)
+    {
+        sum = sum + i;
+    }
+        cout<<"Sum is :"<<sum<<endl;
+
+
+    //Q-5.
+    int num = 5;
+    do
+    {
+        cout<<num<<endl;
+        num++;
+    }while(num < 5);
+    
+    //Q-6.
+    int num = 5;
+    while(num < 10){
+        cout<<num<<endl;
+        num++;
+    }
+
+
+    //Q-7.Print the square of numbers from 1 to 10.
+    for(int i = 1; i < 11; i++){
+        cout<<pow(i, 2)<<endl;
+    }
+    
+    //Q-8.Print the cube of number from 1 to 20.
+    for(int i = 1; i < 21; i++){
+        cout<<pow(i , 3)<<endl;
+    }
+
+
+    //Q-9.
+    int number;
+    cout<<"Enter the palindrome number : "<<endl;
+    cin>>number;
+
+    return 0;
+}
+
