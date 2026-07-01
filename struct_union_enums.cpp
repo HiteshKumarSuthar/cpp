0a2c528ed479cccd9b72312c49e547d15f0bf714
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-07-01 23:23:45 +0530

    update code

---
+++ struct_union_enums.cpp
@@ -0,0 +1,68 @@
+/*----STRUCTURES----*/
+// #include<iostream>
+// using namespace std;
+
+// typedef struct employee
+// {
+//     int eID;
+//     string Domain;
+//     char SDEtype;
+//     float salary;
+// } ep;
+
+// int main(){
+//     ep Hitesh;
+//     Hitesh.eID = 11;
+//     Hitesh.Domain = "DataScientist";
+//     Hitesh.salary = 1500000;
+
+//     cout<<"Hitesh"<<endl;
+//     cout<<"The eID is : "<<Hitesh.eID<<endl;
+//     cout<<"The Domain is : "<<Hitesh.Domain<<endl;
+//     cout<<"The salary is : "<<Hitesh.salary<<endl;
+    
+//     return 0;
+// }
+
+
+/*----UNION----*/
+// #include<iostream>
+// using namespace std;
+
+// union money
+// {
+//     int ruppes;
+//     char doller;
+//     float pounds;
+// };
+
+// int main(){
+//     union money m1;
+//     m1.ruppes = 95;
+//     m1.doller = 'D';
+//     m1.pounds = 126.11;
+
+//     cout<<"ruppes is : "<<m1.ruppes<<endl;
+//     cout<<"doller is : "<<m1.doller<<endl;
+//     // cout<<"pounds is : "<<m1.pounds<<endl;
+//     return 0;
+// }
+
+
+/*----ENUMS----*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    enum Subject{python, java, react};
+    Subject.s1 = react;
+    Subject.s2 = python;
+    cout<<python<<endl;
+    cout<<s1<<endl;
+    cout<<s2;
+
+
+    return 0;
+}
+
+ 
